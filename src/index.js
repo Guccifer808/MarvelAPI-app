51cdc2669f0144aee67e2d9a1a90b19e987baee1
G808 <hucciallstars@gmail.com>
2022-12-04 01:04:36 +0200

    RandomChar component, service improvements

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/App";
-import MarvelService from "./services/MarvelService";
 
 import "./style/style.scss";
-
-//creating new exemplar or class
-const marvelService = new MarvelService();
-
-//using it with method
-//getting array of results
-marvelService
-  .getAllCharacters()
-  .then((res) => res.data.results.forEach((item) => console.log(item.name)));
 
 ReactDOM.render(
   <React.StrictMode>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -20,14 +20,32 @@ class MarvelService {
   // API requests
 
   // All Characters
-  getAllCharacters = () => {
-    return this.getResources(
-      `${this.apiBase}characters?limit=9&offset=205&${this._apiKey}`
+  getAllCharacters = async () => {
+    const res = await this.getResources(
+      `${this._apiBase}characters?limit=9&offset=205&${this._apiKey}`
     );
+    // receiving obj and mapping
+    return res.data.results.map(this._transformCharacter);
   };
   // One Character by id
-  getCharacter = (id) => {
-    return this.getResources(`${this.apiBase}characters/${id}?${this._apiKey}`);
+  // improved w saving into const and async
+  getCharacter = async (id) => {
+    const res = await this.getResources(
+      `${this._apiBase}characters/${id}?${this._apiKey}`
+    );
+
+    return this._transformCharacter(res.data.results[0]);
+  };
+
+  // method to transform response obj into more suitable for us object to work w updateChar()
+  _transformCharacter = (char) => {
+    return {
+      name: char.name,
+      description: char.description,
+      thumbnail: `${char.thumbnail.path}.${char.thumbnail.extension}`,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    };
   };
 }
 
